8e6506265025a0084ea5e24ece268629afaa1f6d
Mohan Krishna <85697913+mohankrishna02@users.noreply.github.com>
2023-05-10 22:24:35 +0530

    Scenerio-4 file added

---
+++ src/pack/Scenerio4.scala
@@ -0,0 +1,35 @@
+package pack
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions._
+object Scenerio4 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("test")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+
+    val df = Seq(
+      (1, "Mark Ray", "AB"),
+      (2, "Peter Smith", "CD"),
+      (1, "Mark Ray", "EF"),
+      (2, "Peter Smith", "GH"),
+      (2, "Peter Smith", "CD"),
+      (3, "Kate", "IJ")).toDF("custid", "custname", "address")
+    df.show()
+
+    //Through SQL
+    df.createOrReplaceTempView("custtab")
+
+    spark.sql("select custid,custname,collect_set(address) as address from custtab group by custid,custname order by custid").show()
+
+    //Through DSL
+
+    val finaldf = df.groupBy("custid", "custname").agg(collect_set("address").as("address")).orderBy("custid").show()
+
+  }
+}
