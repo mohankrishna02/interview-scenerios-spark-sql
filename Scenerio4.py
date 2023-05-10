0090131b46373c8944e1e29e17a30a11bad14cd1
Mohan Krishna <85697913+mohankrishna02@users.noreply.github.com>
2023-05-10 22:23:23 +0530

    Scenerio-4 file added PySpark

---
+++ Scenerio4.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql.window import *
+
+conf = SparkConf().setMaster("local[*]").setAppName("Scenerio-3")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+spark = SparkSession.builder.getOrCreate()
+data = [(1, "Mark Ray", "AB"),
+        (2, "Peter Smith", "CD"),
+        (1, "Mark Ray", "EF"),
+        (2, "Peter Smith", "GH"),
+        (2, "Peter Smith", "CD"),
+        (3, "Kate", "IJ")]
+myschema = ["custid", "custname", "address"]
+df = spark.createDataFrame(data, schema=myschema)
+df.show()
+
+# Through SQL
+df.createOrReplaceTempView("custtab")
+
+spark.sql(
+    "select custid,custname,collect_set(address) as address from custtab group by custid,custname order by custid").show()
+
+# Through DSL
+finaldf = df.groupBy("custid", "custname").agg(collect_set("address").alias("address")).orderBy("custid").show()
