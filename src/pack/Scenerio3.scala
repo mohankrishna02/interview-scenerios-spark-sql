20bea9081f45fc528094bb51f4d50b61d31cb766
Mohan Krishna <85697913+mohankrishna02@users.noreply.github.com>
2023-05-11 09:37:20 +0530

    Update Scenerio3.scala

--- src/pack/Scenerio3.scala
+++ src/pack/Scenerio3.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions._
 object Scenerio3 {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local").setAppName("test")
+    val conf = new SparkConf().setMaster("local").setAppName("scenerio-3")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
     val spark = SparkSession.builder().getOrCreate()
